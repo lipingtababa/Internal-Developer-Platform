cb9c872927df284ce2e4299523592247da26c3d9
Ma Chi <hellomarch@gmail.com>
2024-01-14 20:52:42 +0100

    pass app_name and stage between workflows

--- resources_per_app/variables.tf
+++ resources_per_app/variables.tf
@@ -5,23 +5,19 @@ provider "aws" {
 variable "app_name" {
   description = "Name of the application"
   type        = string
-  default     = "watcher"
 }
 
 variable "stage" {
   description = "Stage of the application"
   type        = string
-  default     = "production"
 }
 
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"
 }
 
 variable "aws_account" {
   description = "AWS account"
   type        = string
-  default     = "339713007259"
 }
